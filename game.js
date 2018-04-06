ef702d553982df8ce38b3736e2cdb97402f2ff10
catloverlisa <35940184+catloverlisa@users.noreply.github.com>
2018-04-05 21:17:38 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -7,14 +7,14 @@
 
 var mouseDown = false;
 var firstKey = true;
-var version = 1;
+var level = 1;
 var maxLevel = 5;
 
 var boardWidth = 5;
 var currentX = 0;
 var currentY = 0;
 
-// Various possible states of the beads
+// possible bead states
 var END_BEAD = 2;
 var ENTERED = 1;
 var NOT_ENTERED = 0;
@@ -27,13 +27,18 @@ function loseStuff() {
 
 function winStuff() {
     PS.data(PS.ALL, PS.ALL, 1);     // sets data to 1 so mouse does not change bead colors
-    PS.color(PS.ALL, PS.ALL, PS.COLOR_RED);
+    //PS.color(PS.ALL, PS.ALL, 150, 200, 0));
+    PS.color(PS.ALL, PS.ALL, 133, 153, 0);
     PS.statusText("You solved all the puzzles!");
     PS.audioPlay("fx_tada");
 }
 
 function markEntered(x, y) {
-    PS.color(x, y, PS.COLOR_RED);
+    //PS.color(x, y, PS.color(x, y, 255, 100, 0));  //wine
+    PS.color(x, y, PS.color(x, y, 211, 54, 130));   //magenta
+    //PS.color(x, y, PS.color(x, y, 220, 50, 47));    //red
+    //PS.color(x, y, PS.color(x, y, 203, 75, 22));    //orange
+    //PS.color(x, y, PS.color(x, y, 38, 139, 210));   //blue
     PS.data(x, y, ENTERED);
 }
 
@@ -44,7 +49,7 @@ function markStart(x, y) {
 }
 
 function markEnd(x, y) {
-    PS.color(x, y, PS.COLOR_BLACK);
+    PS.color(x, y, PS.color(x, y, 133, 153, 0));
     PS.glyph(x, y, "E");
     PS.data(x, y, END_BEAD);
     PS.glyphColor(x, y, PS.COLOR_WHITE);
@@ -70,8 +75,8 @@ function checkWin() {
     if(count > 0) {      // lose if not all beads are entered/red
         loseStuff();
     } else {            // move on to next level
-        if(version < 5) {
-            version++;
+        if(level < 5) {
+            level++;
         }
         PS.init();
         PS.audioPlay("fx_ding");
@@ -104,58 +109,76 @@ PS.init = function( system, options ) {
     mouseDown = false;
     firstKey = true;
 
-	if(version < 5) {    // 5x5 grid size for levels 1-4
+	if(level < 5) {    // 5x5 grid size for levels 1-4
         PS.gridSize(5,5);
     }
-    if(version >= 5) {   // for more levels with larger grids
+    if(level >= 5) {   // for more levels with larger grids
         PS.gridSize(7,7);
     }
 
-	PS.gridColor(175, 175, 215);
-    PS.color(PS.ALL, PS.ALL, 255, 255, 255);
-	PS.statusColor( PS.COLOR_BLACK );
+	PS.gridColor(238, 232, 213);
+    PS.gridShadow(true, PS.COLOR_GRAY);
+    PS.color(PS.ALL, PS.ALL, 238, 232, 213);
+	PS.statusColor(88, 110, 117);   //dark gray
+    PS.statusColor(PS.COLOR_BLACK);
 	PS.border( PS.ALL, PS.ALL, 0 );     // no border
 
     PS.data(PS.ALL, PS.ALL, 0);         // resets each bead to zero for new levels
 
-
-    markEntered(0, 4);
-    markEntered(1, 0);
-    markEntered(2, 0);
-    markEntered(3, 0);
-    markEntered(2, 3);
-    markEntered(4, 0);
-
-    if (version === 1) {
+    if (level === 1) {
+        markEntered(0, 4);
+        markEntered(1, 0);
+        markEntered(2, 0);
+        markEntered(3, 0);
+        markEntered(2, 3);
+        markEntered(4, 0);
         markStart(0, 0);
         markEnd(2, 2);
     }
-    if (version === 2) {
+    if (level === 2) {
+        markEntered(0, 4);
+        markEntered(1, 0);
+        markEntered(2, 0);
+        markEntered(3, 0);
+        markEntered(2, 3);
+        markEntered(4, 0);
         markStart(0, 0);
         markEnd(4, 4);
     }
-    if (version === 3) {
+    if (level === 3) {
+        markEntered(0, 4);
+        markEntered(1, 0);
+        markEntered(2, 0);
+        markEntered(3, 0);
+        markEntered(2, 3);
+        markEntered(4, 0);
         markStart(0, 0);
         markEnd(2, 4);
     }
-    if (version === 4) {
+    if (level === 4) {
+        markEntered(0, 4);
+        markEntered(1, 0);
+        markEntered(2, 0);
+        markEntered(3, 0);
+        markEntered(2, 3);
+        markEntered(4, 0);
         markStart(0, 0);
         markEnd(3, 1);
     }
-    if(version ===1) {
-        PS.statusText( "Drag to fill the Grid! Level " + version);
+    if(level ===1) {
+        PS.statusText( "Drag to fill the Grid! Level " + level);
     }
-    else if(version === 2) {
-        PS.statusText("Or use arrow keys! Level " + version);
+    else if(level === 2) {
+        PS.statusText("Or use arrow keys! Level " + level);
     }
-    else if(version === 3) {
-        PS.statusText("You got the hang of it. Level " + version)
+    else if(level === 3) {
+        PS.statusText("You got the hang of it. Level " + level)
     }
-    else if(version > 3 && version < maxLevel) {
-        PS.statusText("Level " + version);
+    else if(level > 3 && level < maxLevel) {
+        PS.statusText("Level " + level);
     }
 
-    if(version === maxLevel) {
+    if(level === maxLevel) {
         winStuff();
     }
 };
@@ -179,11 +202,10 @@ PS.enter = function( x, y, data, options ) {
 	"use strict";
 
     if(mouseDown === false && PS.data(x,y) === NOT_ENTERED) {       // turns bead gray when hovering over
-        PS.color(x, y, 225, 225, 225);
+        PS.color(x, y, 218, 212, 193);
     }
     else if (mouseDown === true && PS.data(x,y) !== ENTERED) {      // enables dragging
-        PS.color(x, y, PS.COLOR_RED);
-        PS.data(x, y, ENTERED);
+        markEntered(x, y);
         PS.audioPlay("fx_pop");
     }
     else if (mouseDown === true && PS.data(x,y) === ENTERED) {     // lose if player tries to go back over a bead
@@ -197,7 +219,7 @@ PS.exit = function( x, y, data, options ) {
     "use strict";
 
     if(mouseDown === false && PS.data(x,y) === NOT_ENTERED) {       // removes gray from previous bead
-        PS.color(x, y, 255, 255, 255);
+        PS.color(x, y, 238, 232, 213);
     }
 };
 
