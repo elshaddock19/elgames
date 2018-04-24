5f95238e78bbc63d1763efaac770fc9573bc7a51
elshaddock19 <30415035+elshaddock19@users.noreply.github.com>
2018-04-24 13:05:45 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -5,256 +5,330 @@
 /*jslint nomen: true, white: true */
 /*global PS */
 
-var db = null;
-
+var mouseDown = false;
+var firstKey = true;
 var level = 1;
 var maxLevel = 3;
-var boardWidth = 15;
+
+var boardWidth = 0;
 var currentX = 0;
 var currentY = 0;
-var createdTimer = false;
 
-var enemyXs = [];
-var enemyYs = [];
+// possible bead states
+var END_BEAD = 2;
+var ENTERED = 1;
+var NOT_ENTERED = 0;
 
-var timerID;
-
-var finalize = function () {
-    if(!createdTimer) {
-        timerID = PS.timerStart(60, myTimer);
-        createdTimer = true;
-    }
-};
-
-function myTimer() {
-    console.log("tick");
-    for(var counter = 0; counter < enemyXs.length; counter++) {
-        var enemyX = enemyXs[counter];
-        var enemyY = enemyYs[counter];
-        moveEnemy(enemyX, enemyY);
-    }
-};
-
-function playerBead(x, y) {
-    PS.color(x, y, 50, 50, 50);
-    PS.data(x, y, "player");
-}
-
-function move(x, y) {
-    if (x < 0) {
-        playerBead(x + 1, y);
-    } else if (y < 0) {
-        playerBead(x, y + 1);
-    } else if (x > boardWidth - 1) {
-        playerBead(x - 1, y);
-    } else if (y > boardWidth - 1) {
-        playerBead(x, y - 1);
-    } else {
-        if (PS.data(x, y) === "enemy") {
-            lose();
-            //console.log(5);
-        } else {
-            playerBead(x, y);
-            currentX = x;
-            currentY = y;
-            PS.audioPlay("fx_pop");
-        }
-    }
-}
-
-function massDown() {
-
-}
-
-function massUp() {
-
-}
-
-function updateEnemyPosition(x, y, newX, newY) {
-    PS.color(newX, newY, 250, 250, 250);
-    PS.data(newX, newY, "enemy");
-
-    var counter = 0;
-    for(counter = 0; counter < enemyXs.length; counter++) {
-        var enemyX = enemyXs[counter];
-        var enemyY = enemyYs[counter];
-        if(enemyX === x && enemyY === y) {
-            enemyXs[counter] = newX;
-            enemyYs[counter] = newY;
-        }
-    }
-}
-
-function newEnemy(x, y) {
-    PS.color(x, y, 250, 250, 250);
-    PS.data(x, y, "enemy");
-    enemyXs.push(x);
-    enemyYs.push(y);
-}
-
-function moveEnemy(x, y) {
-    xInc = getRandomMove(-1, 1);
-    yInc = getRandomMove(-1, 1);
-    if (isMoveValid(x + xInc, y + yInc)) {
-        reset(x, y);
-        updateEnemyPosition(x, y, x + xInc, y + yInc);
-    } else {
-        moveEnemy(x, y);
-    }
-}
-
-function getRandomMove(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function isMoveValid( newX, newY) {
-    if(newX > 0 && newX < boardWidth - 1 &&
-            newY > 0 && newY < boardWidth - 1) {
-        if(newX != currentX && newY != currentY) {
-            if(!isEnemyHere(newX, newY)) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function isEnemyHere(x, y) {
-    for(var counter = 0; counter < enemyXs.length; counter++) {
-        var enemyX = enemyXs[counter];
-        var enemyY = enemyYs[counter];
-        if(enemyX === x && enemyY === y) {
-            return true;
-        }
-    }
-    return false;
-}
-
-/*
-function checkWin() {
-    var count = 0;
-    for (var xNum = 0; xNum < 5; xNum++) {
-        for (var yNum = 0; yNum < 5; yNum++) {
-            if (PS.data(xNum, yNum) !== ENTERED) {     // if not all 
-beads are entered (set to 1), increase count
-                count++;
-            }
-        }
-    }
-    if (count > 0) {      // lose if not all beads are entered/red
-        lose();
-    } else {            // move on to next level
-        if (level < maxLevel) {
-            level++;
-        }
-        PS.init();
-        PS.audioPlay("fx_ding");
-    }
-}
-*/
-
-function lose() {
-    if (db && PS.dbValid(db)) {
-        PS.dbEvent(db, "gameover", true);
-        PS.dbSend(db, "bmoriarty", {discard: true});
-        db = null;
-    }
-
+// Lose Condition
+function loseStuff() {
     PS.init();
     PS.audioPlay("fx_hoot");
 }
 
+// Win Condition
 function winStuff() {
+    PS.data(PS.ALL, PS.ALL, 1);     // sets data to 1 so mouse does not change bead colors
     PS.color(PS.ALL, PS.ALL, 133, 153, 0);
+    PS.statusText("You ate the grid!");
     PS.audioPlay("fx_tada");
-    PS.color(10, 10, 10);       // grid color same as bg color
 }
 
-function reset(x, y) {
-    PS.data(x, y);
-    PS.color(x, y, 175, 175, 175);
+// Check if bead entered and apply effect
+
+function markEntered(x, y) {
+    PS.scale(13, 4, 75);                // health bead medium
+    PS.radius(13, 4, 50);
+    PS.color(13, 4, 200, 200, 200);
+    PS.data(x, y, ENTERED);
 }
 
-PS.init = function (system, options) {
-    PS.gridSize(15, 15);
-    PS.gridColor(10, 10, 10);                       // dark background
-    PS.gridShadow(true, PS.COLOR_GRAY);             // glowing effect
-    PS.color(PS.ALL, PS.ALL, 175, 175, 175);        // light board
-    PS.statusColor(180, 180, 180);                  // light gray text over dark bg
-    PS.statusText("Onward");                        // title
-    PS.border(PS.ALL, PS.ALL, 0);                   // no border
-    PS.bgAlpha(PS.ALL, PS.ALL, PS.ALPHA_OPAQUE);    // opaque bg
-    PS.bgColor(PS.ALL, PS.ALL, 175, 175, 175);      // same color as board
+// Mark Starting point
+function markStart(x, y) {
+    PS.glyph(x, y, "S");
+    PS.glyphColor(x, y, 100, 100, 100);
+    currentX = x;
+    currentY = y;
+}
 
-    // resets data from previous game
-    PS.data(PS.ALL, PS.ALL);
-    currentX = 7;
-    currentY = 7;
-    level = 1;
-    playerBead(currentX, currentY);
+// Mark End point
+function markEnd(x, y) {
+    PS.color(x, y, PS.color(x, y, 133, 153, 0));
+    PS.glyph(x, y, "E");
+    PS.data(x, y, END_BEAD);
+    PS.glyphColor(x, y, 238, 232, 213);
+}
 
-    enemyXs = [];
-    enemyYs = [];
+function notEntered(x, y) {
+    return PS.data(x, y) !== ENTERED;
+}
 
-    // creates enemies
-    newEnemy(5, 3);
-    newEnemy(5, 2);
-    newEnemy(10, 10);
-    newEnemy(11, 10);
-    newEnemy(12, 10);
-    newEnemy(13, 10);
-    newEnemy(14, 10);
-
-    if (db) {
-        db = PS.dbInit(db, {login: finalize});
-        if (db === PS.ERROR) {
-            db = null;
-        }
+// Check if at Start point
+function isStart(x, y) {
+    if(x === 0 && y === 0) {
+        return true;
     } else {
-        finalize();
+        return false;
     }
-};
+}
 
+// Check if at End point
+function isEnd(x, y) {
+    return PS.data(x, y) === END_BEAD;
+}
 
-// At major game events (end of a level,
-// major victory, etc), add this bit of code
-// to record the event in the database.
+// Check win conditions and increment level
+function checkWin() {
+    if(isEnd){
+        if(level < maxLevel) {
+            level++;
+        }
+        PS.init();
+        PS.audioPlay("fx_ding");
 
+    }
 
-// At the end of your game (or the
-// end of the last available level),
-// install this code to send the data.
+}
 
+// Make player movable
+function move(x, y) {
+    if(x < 0 || y < 0 || x > boardWidth - 1 || y > boardWidth - 1) {
+        return null; // do nothing
+    } else {
+        if(notEntered(x, y)) {
+            if(isEnd(x, y)) {
+                markEntered(x, y);
+                currentBead(x, y);
 
-PS.keyDown = function (key, shift, ctrl, options) {
+            } else {
+                markEntered(x, y);
+                currentBead(x, y);
+                currentX = x;
+                currentY = y;
+                PS.audioPlay("fx_pop");
+            }
+        }
+    }
+}
+
+//Distinguish player bead from markEntered bead
+function currentBead(x, y) {
+    PS.color(x, y, 0, 0, 0);
+
+}
+
+// Start Game
+PS.init = function( system, options ) {
     "use strict";
-    if (key === PS.KEY_ARROW_DOWN) {
-        PS.color(currentX, currentY, 175, 175, 175);
-        move(currentX, currentY + 1);
+
+    mouseDown = false;
+    firstKey = true;
+
+    if(level < 5) {    // 5x5 grid size for levels 1-4
+        PS.gridSize(15,15);
+    }/*
+    if(level >= 5 && level < 7)    // for more levels with larger grids
+    {
+        PS.gridSize(7,7);
     }
-    else if (key === PS.KEY_ARROW_UP) {
-        PS.color(currentX, currentY, 175, 175, 175);
-        move(currentX, currentY - 1);
+    if(level >= 7)    // for more levels with larger grids
+    {
+        PS.gridSize(9,9);
+    }*/
+
+    PS.gridColor(10, 10, 10);
+    PS.gridShadow(true, PS.COLOR_GRAY);
+    PS.color(PS.ALL, PS.ALL, 175, 175, 175);
+    //PS.statusColor(88, 110, 117);                   //dark gray
+    PS.statusColor(180, 180, 180);                   //light gray
+    //PS.statusColor(100, 100, 100);
+    PS.statusText("Onward");
+    PS.border( PS.ALL, PS.ALL, 0 );                 // no border
+    PS.bgAlpha( PS.ALL, PS.ALL, PS.ALPHA_OPAQUE );  // opaque bg
+    PS.bgColor( PS.ALL, PS.ALL, 175, 175, 175 );    // same color as board
+
+
+
+    PS.data(PS.ALL, PS.ALL, 0);         // resets each bead to zero for new levels
+
+    if (level === 1) {
+        markEntered(13, 4);
+        markStart(0, 0);
+        markEnd(2, 2);
+        boardWidth = 15;
     }
-    else if (key === PS.KEY_ARROW_LEFT) {
-        PS.color(currentX, currentY, 175, 175, 175);
-        move(currentX - 1, currentY);
+    if (level === 2) {
+        markEntered(13, 4);
+        markStart(0, 0);
+        markEnd(4, 4);
+        boardWidth = 15;
     }
-    else if (key === PS.KEY_ARROW_RIGHT) {
-        PS.color(currentX, currentY, 175, 175, 175);
-        move(currentX + 1, currentY);
+    /*
+    if (level === 3) {
+        markEntered(0, 4);
+        markEntered(1, 0);
+        markEntered(2, 0);
+        markEntered(3, 0);
+        markEntered(2, 3);
+        markEntered(4, 0);
+        markStart(0, 0);
+        markEnd(2, 4);
+        boardWidth = 15;
+    }
+    if (level === 4) {
+        markEntered(0, 4);
+        markEntered(1, 0);
+        markEntered(2, 0);
+        markEntered(3, 0);
+        markEntered(2, 3);
+        markEntered(4, 0);
+        markStart(0, 0);
+        markEnd(3, 1);
+        boardWidth = 15;
+    }
+    if (level === 5) {
+        markEntered(0, 4);
+        markEntered(1, 0);
+        markEntered(2, 0);
+        markEntered(3, 0);
+        markEntered(2, 3);
+        markEntered(4, 0);
+        markStart(0, 0);
+        markEnd(3, 1);
+        boardWidth = 15;
+    }
+    if (level === 6) {
+        markEntered(0, 4);
+        markEntered(1, 0);
+        markEntered(2, 0);
+        markEntered(3, 0);
+        markEntered(2, 3);
+        markEntered(4, 0);
+        markEntered(5, 0);
+        markEntered(6, 0);
+        markEntered(6, 1);
+        markEntered(6, 2);
+        markEntered(6, 3);
+        markEntered(5, 5);
+        markStart(0, 0);
+        markEnd(5, 3);
+        boardWidth = 15;
+    }
+    if (level === 7) {
+        markEntered(0, 4);
+        markEntered(1, 0);
+        markEntered(2, 0);
+        markEntered(3, 0);
+        markEntered(2, 3);
+        markEntered(4, 0);
+        markEntered(4, 4);
+        markEntered(4, 5);
+        markEntered(5, 4);
+        markEntered(5, 5);
+        markEntered(5, 7);
+        markEntered(5, 0);
+        markEntered(6, 0);
+        markEntered(6, 1);
+        markEntered(6, 2);
+        markEntered(6, 3);
+        markEntered(6, 4);
+        markEntered(6, 5);
+        markStart(0, 0);
+        markEnd(5, 3);
+        boardWidth = 15;
+    }*/
+
+
+    if(level === maxLevel) {
+        winStuff();
     }
 };
 
-// Add the code below to the
-// PS.shutdown() event handler at the
-// bottom of game.js to save your
-// data in case the player closes
-// the browser before finishing.
+PS.touch = function( x, y, data, options ) {
+    "use strict";
+    if(x === 0 && y === 0) {
+        mouseDown = true;
+        //markEntered(x, y);
+        currentBead(x, y);          // make darker tile move with mouse/keys
+    }
+};
 
-PS.shutdown = function (options) {
-    if (db && PS.dbValid(db)) {
-        PS.dbEvent(db, "shutdown", true);
-        PS.dbSend(db, "bmoriarty", {discard: true});
+PS.release = function( x, y, data, options ) {
+    "use strict";
+    mouseDown = false;
+
+
+};
+
+PS.enter = function( x, y, data, options ) {
+
+    // console.log("Hello there"); // for debugging
+
+    if(markEntered(13,4) === ENTERED){
+        PS.scale(x, y, 100);
+    }
+    checkWin();
+};
+
+PS.exit = function( x, y, data, options ) {
+    "use strict";
+    if(mouseDown === true) {
+        PS.color(x, y, PS.color(x, y, 175, 175, 175));               // resets color
+    }
+    if(mouseDown === false && PS.data(x,y) === NOT_ENTERED) {       // removes gray from previous bead
+        PS.color(x, y, 175, 175, 175);
+    }
+    if(markEntered(13,4) === ENTERED){
+        PS.scale(x, y, 100);
+
+    }
+
+};
+
+PS.exitGrid = function( options ) {
+    "use strict";
+
+
+};
+
+PS.keyDown = function( key, shift, ctrl, options ) {
+    "use strict";
+    if(markEntered(13,4) === ENTERED){
+        PS.scale(x, y, 100);
+    }
+    if(key === PS.KEY_ARROW_DOWN) {
+        if(firstKey) {
+            move(currentX, currentY);
+            firstKey = false;
+        } else {
+            PS.color(currentX, currentY, 175, 175, 175);
+            move(currentX, currentY + 1);
+        }
+    }
+    else if(key === PS.KEY_ARROW_UP) {
+        if(firstKey) {
+            move(currentX, currentY);
+            firstKey = false;
+        } else {
+            PS.color(currentX, currentY, 175, 175, 175);
+            move(currentX, currentY - 1);
+        }
+    }
+    else if(key === PS.KEY_ARROW_LEFT) {
+        if(firstKey) {
+            move(currentX, currentY);
+            firstKey = false;
+        } else {
+            PS.color(currentX, currentY, 175, 175, 175);
+            move(currentX - 1, currentY);
+        }
+    }
+    else if(key === PS.KEY_ARROW_RIGHT) {
+        if(firstKey) {
+            move(currentX, currentY);
+            firstKey = false;
+        } else {
+            PS.color(currentX, currentY, 175, 175, 175);
+            move(currentX + 1, currentY);
+        }
     }
 };
