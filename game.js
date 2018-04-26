2f4154ec089635d8a8b6c8e3160c741eca84bc09
catloverlisa <35940184+catloverlisa@users.noreply.github.com>
2018-04-26 01:05:34 -0400

    3 levels with a few bugs
    - moving enemies
    - checkpoints and end bead

--- game.js
+++ game.js
@@ -1,49 +1,117 @@
 // ELGames
-// Puzzle
-// game.js for Perlenspiel 3.2
+// Lisa Liao (lliao) &
+// Ed Shaddock (elshaddock)
+// Onward: an Abstract Game
 
+// game.js for Perlenspiel 3.2
 /*jslint nomen: true, white: true */
 /*global PS */
 
-var db = null;
+var db = "Onward";
 
 var level = 1;
 var maxLevel = 3;
 var boardWidth = 15;
-var currentX = 0;
-var currentY = 0;
+var currentX = 7;
+var currentY = 7;
 var createdTimer = false;
+var currentLevelEndX;
+var currentLevelEndY;
+var checkpointCount = 0;
 
 var enemyXs = [];
 var enemyYs = [];
-var bulletXs = [];
-var bulletYs = [];
+var checkpointsX = [];
+var checkpointsY = [];
+
+var PLAYER = "player";
+var ENEMY = "enemy";
+var START = "start";
+var END = "end";
+var CHECKPOINT = "checkpoint";
 
 var timerID;
 
 var finalize = function () {
-    if(!createdTimer) {
-        timerID = PS.timerStart(60, myTimer);
+    if (!createdTimer) {
+        timerID = PS.timerStart(80, myTimer);
         createdTimer = true;
     }
+
+    // resets data from previous level
+    PS.data(PS.ALL, PS.ALL);
+    currentX = 7;
+    currentY = 7;
+    enemyXs = [];
+    enemyYs = [];
+    checkpointCount = 0;
+
+    // creates start, end, and checkpoints for each level
+    if (level === 1) {
+        createCheckpoint(5, 5);
+        createEnd(14, 8);
+    } else if (level === 2) {
+        createCheckpoint(4, 8);
+        createCheckpoint(8, 4);
+        createEnd(14, 12);
+    } else if (level === 3) {
+        createCheckpoint(6, 7);
+        createCheckpoint(4, 2);
+        createCheckpoint(10, 11);
+        createEnd(14, 1);
+    }
+
+    // creates enemies
+    newEnemy(5, 3);
+    newEnemy(5, 2);
+    newEnemy(10, 10);
+    newEnemy(11, 10);
+    newEnemy(12, 10);
+    newEnemy(13, 10);
+    newEnemy(14, 10);
+    newEnemy(7, 10);
+    newEnemy(8, 11);
+    newEnemy(8, 3);
+    newEnemy(6, 4);
+
 };
 
 function myTimer() {
     console.log("tick");
-    for(var counter = 0; counter < enemyXs.length; counter++) {
+    for (var counter = 0; counter < enemyXs.length; counter++) {
         var enemyX = enemyXs[counter];
         var enemyY = enemyYs[counter];
         moveEnemy(enemyX, enemyY);
-        var bulletX = bulletXs[counter];
-        var bulletY = bulletYs[counter];
-        moveBullet(bulletX, bulletY);
     }
+}
 
-};
+function createCheckpoint(x, y) {
+    PS.data(x, y, CHECKPOINT);
+    PS.border(x, y, 5);
+    PS.borderColor(x, y, 150, 150, 150);
+    checkpointsX.push(x);
+    checkpointsY.push(y);
+}
+
+function isPlayerAtCheckpoint(x, y) {
+    return PS.data(x, y) === CHECKPOINT;
+}
+
+function createEnd(x, y) {
+    PS.data(x, y, END);
+    PS.border(x, y, 5);
+    PS.borderColor(x, y, 100, 100, 100);
+    currentLevelEndX = x;
+    currentLevelEndY = y;
+}
+
+function isPlayerAtEnd(x, y) {
+    return PS.data(x, y) === END;
+}
 
 function playerBead(x, y) {
     PS.color(x, y, 50, 50, 50);
-    PS.data(x, y, "player");
+    PS.data(x, y, PLAYER);
 }
 
 function move(x, y) {
@@ -56,94 +124,64 @@ function move(x, y) {
     } else if (y > boardWidth - 1) {
         playerBead(x, y - 1);
     } else {
-        if (PS.data(x, y) === "enemy") {
-            lose();
-            //console.log(5);
-        } else {
-            playerBead(x, y);
-            currentX = x;
-            currentY = y;
-            PS.audioPlay("fx_pop");
+        // restarts level if player enters enemy bead
+        if (PS.data(x, y) === ENEMY) {
+            restart();
+        } else if (isPlayerAtEnd(x, y)) {   // checks if necessary num of checkpoints have been passed
+            if (level === 1) {
+                if (checkpointCount === 1) {
+                    levelUp();
+                }
+            } else if (level === 2) {
+                if (checkpointCount === 2) {
+                    levelUp();
+                }
+            } else if (level === 3) {
+                if (checkpointCount === 3) {
+                    win();
+                }
+            }
+        } else if (isPlayerAtCheckpoint(x, y)) {
+            console.log("checkpoint");
+            checkpointCount++;
+            reset(x, y);
+
+            // send data point
+            if (db && PS.dbValid(db)) {
+                PS.dbEvent(db, "gameover", true);
+                PS.dbSend(db, "bmoriarty", {discard: true});
+                db = null;
+            }
         }
+        playerBead(x, y);
+        currentX = x;
+        currentY = y;
+        PS.audioPlay("fx_pop");
     }
-}
-
-function massDown() {
-
-}
-
-function massUp() {
-
 }
 
 function updateEnemyPosition(x, y, newX, newY) {
     PS.color(newX, newY, 250, 250, 250);
-    PS.data(newX, newY, "enemy");
+    PS.data(newX, newY, ENEMY);
+    PS.scale(newX, newY, 50);
+    PS.scale(x, y);
 
-    var counter = 0;
-    for(counter = 0; counter < enemyXs.length; counter++) {
+    for (counter = 0; counter < enemyXs.length; counter++) {
         var enemyX = enemyXs[counter];
         var enemyY = enemyYs[counter];
-        if(enemyX === x && enemyY === y) {
+        if (enemyX === x && enemyY === y) {
             enemyXs[counter] = newX;
             enemyYs[counter] = newY;
         }
     }
 }
 
-function updateBulletPosition(x, y, newX, newY) {
-    PS.color(newX, newY, 250, 250, 250);
-    PS.data(newX, newY, "bullet");
-
-    var counter = 0;
-    for(counter = 0; counter < bulletXs.length; counter++) {
-        var bulletX = bulletXs[counter];
-        var bulletY = bulletYs[counter];
-        if(bulletX === x && bulletY === y) {
-            bulletXs[counter] = newX;
-            bulletYs[counter] = newY;
-        }
-    }
-}
-
 function newEnemy(x, y) {
     PS.color(x, y, 250, 250, 250);
-    PS.data(x, y, "enemy");
+    PS.scale(x, y, 50);
+    PS.data(x, y, ENEMY);
     enemyXs.push(x);
     enemyYs.push(y);
-}
-
-function bullet(x, y){
-    PS.color(x, y, 0, 0, 0);
-    PS.scale(x, y, 20);
-    PS.data(x, y, "bullet");
-    bulletXs.push(x);
-    bulletYs.push(y);
-}
-
-function moveBullet(x, y){
-    bulletDirection(x, y);
-    if(isMoveValid(x + xInc, y + yInc)){
-        reset(x, y);
-        updateBulletPosition(x, y, x + xInc, y +yInc);
-    } else {
-        //moveBullet(x, y);
-    }
-}
-
-function bulletDirection(x, y) {
-    if(currentX < x) {
-        xInc = -1;
-    }
-    if(currentX > x) {
-        xInc = 1;
-    }
-    if(currentY < y) {
-        yInc = -1;
-    }
-    if(currentY > y) {
-        yInc = 1;
-    }
 }
 
 function moveEnemy(x, y) {
@@ -161,12 +199,14 @@ function getRandomMove(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function isMoveValid( newX, newY) {
-    if(newX > 0 && newX < boardWidth - 1 &&
+function isMoveValid(newX, newY) {
+    if (newX > 0 && newX < boardWidth - 1 &&
         newY > 0 && newY < boardWidth - 1) {
-        if(newX != currentX && newY != currentY) {
-            if(!isEnemyHere(newX, newY)) {
-                return true;
+        if (newX !== currentX && newY !== currentY) {
+            if (!isEnemyHere(newX, newY)) {
+                if (!isStartHere(newX, newY) && !isEndHere(newX, newY) && !isCheckpointHere(newX, newY)) {
+                    return true;
+                }
             }
         }
     }
@@ -174,58 +214,68 @@ function isMoveValid( newX, newY) {
 }
 
 function isEnemyHere(x, y) {
-    for(var counter = 0; counter < enemyXs.length; counter++) {
+    for (var counter = 0; counter < enemyXs.length; counter++) {
         var enemyX = enemyXs[counter];
         var enemyY = enemyYs[counter];
-        if(enemyX === x && enemyY === y) {
+        if (enemyX === x && enemyY === y) {
             return true;
         }
     }
     return false;
 }
 
-/*
-function checkWin() {
-    var count = 0;
-    for (var xNum = 0; xNum < 5; xNum++) {
-        for (var yNum = 0; yNum < 5; yNum++) {
-            if (PS.data(xNum, yNum) !== ENTERED) {     // if not all
-beads are entered (set to 1), increase count
-                count++;
-            }
-        }
-    }
-    if (count > 0) {      // lose if not all beads are entered/red
-        lose();
-    } else {            // move on to next level
-        if (level < maxLevel) {
-            level++;
-        }
-        PS.init();
-        PS.audioPlay("fx_ding");
-    }
+function isStartHere(x, y) {
+    return PS.data(x, y) === START;
 }
-*/
 
-function lose() {
+function isEndHere(x, y) {
+    return PS.data(x, y) === END;
+}
+
+function isCheckpointHere(x, y) {
+    return PS.data(x, y) === CHECKPOINT;
+}
+
+function levelUp() {
+    level++;
+    finalize();
+}
+
+function restart() {
+    PS.init();
+    PS.audioPlay("fx_hoot");
+
+    //sends data point
     if (db && PS.dbValid(db)) {
         PS.dbEvent(db, "gameover", true);
         PS.dbSend(db, "bmoriarty", {discard: true});
         db = null;
     }
-
-    PS.init();
-    PS.audioPlay("fx_hoot");
 }
 
-function winStuff() {
-    PS.color(PS.ALL, PS.ALL, 133, 153, 0);
+function win() {
+    PS.timerStop(timerID);
+    reset(PS.ALL, PS.ALL);
+    PS.fade(PS.ALL, PS.ALL, 60);
+    PS.color(PS.ALL, PS.ALL, 255, 255, 255);
     PS.audioPlay("fx_tada");
-    PS.color(10, 10, 10);       // grid color same as bg color
+    //PS.color(10, 10, 10);       // grid color same as bg color
+
+    // sends data point
+    if (db && PS.dbValid(db)) {
+        PS.dbEvent(db, "gameover", true);
+        PS.dbSend(db, "bmoriarty", {discard: true});
+        db = null;
+    }
 }
 
 function reset(x, y) {
+    PS.scale(x, y, 100);
+    PS.border(x, y, 0);
+    PS.borderColor(x, y);
     PS.data(x, y);
+    // fade does not work ???
+    //PS.fade(x, y, 60);
     PS.color(x, y, 175, 175, 175);
 }
 
@@ -240,38 +290,7 @@ PS.init = function (system, options) {
     PS.bgAlpha(PS.ALL, PS.ALL, PS.ALPHA_OPAQUE);    // opaque bg
     PS.bgColor(PS.ALL, PS.ALL, 175, 175, 175);      // same color as board
 
-    // resets data from previous game
-    PS.data(PS.ALL, PS.ALL);
-    currentX = 7;
-    currentY = 7;
-    level = 1;
     playerBead(currentX, currentY);
-
-    enemyXs = [];
-    enemyYs = [];
-
-    // creates enemies
-    newEnemy(5, 3);
-    newEnemy(5, 2);
-    newEnemy(10, 10);
-    newEnemy(11, 10);
-    newEnemy(12, 10);
-    newEnemy(13, 10);
-    newEnemy(14, 10);
-    newEnemy(7, 10);
-    newEnemy(8, 11);
-    newEnemy(8, 3);
-    newEnemy(6, 4);
-
-    /*
-    bullet(5, 3);
-    bullet(5, 2);
-    bullet(10, 10);
-    bullet(11, 10);
-    bullet(12, 10);
-    bullet(13, 10);
-    bullet(14, 10);
-    */
 
     if (db) {
         db = PS.dbInit(db, {login: finalize});
@@ -282,17 +301,6 @@ PS.init = function (system, options) {
         finalize();
     }
 };
-
-
-// At major game events (end of a level,
-// major victory, etc), add this bit of code
-// to record the event in the database.
-
-
-// At the end of your game (or the
-// end of the last available level),
-// install this code to send the data.
-
 
 PS.keyDown = function (key, shift, ctrl, options) {
     "use strict";
@@ -314,12 +322,7 @@ PS.keyDown = function (key, shift, ctrl, options) {
     }
 };
 
-// Add the code below to the
-// PS.shutdown() event handler at the
-// bottom of game.js to save your
-// data in case the player closes
-// the browser before finishing.
-
+// sends data if window is closed before game ends
 PS.shutdown = function (options) {
     if (db && PS.dbValid(db)) {
         PS.dbEvent(db, "shutdown", true);
